ee3235a5eb121aaaad9d6c6332fd6ace9e2800d3
Rafinad <rafinad@outlook.com>
2021-05-08 21:31:46 +0300

    Initial Commit

---
+++ LoginScreenPractice/LoginScreenPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenPracticeApp.swift
+//  LoginScreenPractice
+//
+//  Created by Сахар Кубический on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
